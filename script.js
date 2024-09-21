ac1ac72124ea8ada956a6f40476e2bd2aa1a9386
Matanda Phiri <matandaphiri64@gmail.com>
2024-09-21 02:12:20 -0500

    Add css file

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+
+//get buttons
+const buttons = document.querySelector("#buttons");
+const playerInfo = document.querySelector("#playerdets");
+const score = document.querySelector("#scoreCount");
+const showWinner = document.querySelector("#displayWinner");
+
+
+//Add text Content at start of game
+playerInfo.textContent = `Human Computer`;
+
+buttons.addEventListener('click', (e) => {
+    //reset winner display data
+    showWinner.textContent ="";
+
+    const target = e.target;
+
+    switch(target.id){
+        case "rock":
+            playRound("Rock", getComputerChoice());
+            break;
+        case "paper":
+            playRound("Paper", getComputerChoice());
+            break;
+        case "scissors":
+            playRound("Scissors", getComputerChoice());
+            break;
+    }
+});
+
 //This function is used to get the computers play in the game
 function getComputerChoice(){
     //First generate a random number between 1 and 2, 2 inclusive.
@@ -53,7 +83,7 @@ function getHumanChoice(){
             break;
         default:
             humanPlay = "I will never be returned";
-    }
+    }1
 
     //return human's play
     return humanPlay;
@@ -63,43 +93,54 @@ function getHumanChoice(){
 
 let playGame = () => {
     //Declare variables to keep track of scores
-    let humanScore = 0;
-    let computerScore = 0;
 
     let numberOfRounds = 0;
 
-    //This function allows the user to play a single round of the game
-    let playRound = (humanChoice, computerChoice) => {
-        if(humanChoice === computerChoice){
-            console.log("Draw")
-        }else if(humanChoice === "Rock" && computerChoice === "Paper"){
-            console.log("You lose!Paper beats Rock.")
-            computerScore++;
-        }else if(humanChoice === "Scissors" && computerChoice === "Rock"){
-            console.log("You lose!Rock beats Scissors.")
-            computerScore++;
-        }else if(humanChoice === "Paper" && computerChoice === "Scissors"){
-            console.log("You lose!Scissors beats Paper.")
-            computerScore++;
-        }else if(humanChoice === "Paper" && computerChoice === "Rock"){
-            console.log("You win!Paper beats Rock.")
-            humanScore++;
-        }else if(humanChoice === "Rock" && computerChoice === "Scissors"){
-            console.log("You win!Rock beats Scissors.")
-            humanScore++;
-        }else if(humanChoice === "Scissors" && computerChoice === "Paper"){
-            console.log("You win!Scissors beats Paper.")
-            humanScore++;
+//This function allows the user to play a single round of the game
+let playRound = (humanChoice, computerChoice) => {
+    //check for winner
+    if(humanChoice === computerChoice){
+        console.log("Draw")
+    }else if(humanChoice === "Rock" && computerChoice === "Paper"){
+        console.log("You lose!Paper beats Rock.")
+        computerScore++;
+    }else if(humanChoice === "Scissors" && computerChoice === "Rock"){
+        console.log("You lose!Rock beats Scissors.")
+        computerScore++;
+    }else if(humanChoice === "Paper" && computerChoice === "Scissors"){
+        console.log("You lose!Scissors beats Paper.")
+        computerScore++;
+    }else if(humanChoice === "Paper" && computerChoice === "Rock"){
+        console.log("You win!Paper beats Rock.")
+        humanScore++;
+    }else if(humanChoice === "Rock" && computerChoice === "Scissors"){
+        console.log("You win!Rock beats Scissors.")
+        humanScore++;
+    }else if(humanChoice === "Scissors" && computerChoice === "Paper"){
+        console.log("You win!Scissors beats Paper.")
+        humanScore++;
+    }
+
+    //Check first to five and end game
+    if(humanScore === 5 || computerScore === 5){
+        if(humanScore > computerScore){
+            showWinner.textContent = "You Win!";
+        }else if(computerScore > humanScore){
+            showWinner.textContent = "You Lose!";
         }
+        //Reset Scores
+        humanScore = 0;
+        computerScore = 0;  
     }
 
-    while(numberOfRounds < 5){
-        playRound(getHumanChoice(),getComputerChoice());
-        numberOfRounds++;
-    }
-
-    console.log(`Scores\nHuman:${humanScore}\t\tComputer:${computerScore}`);
-
+    score.textContent  = `${humanScore}     :     ${computerScore}`;
+    
 }
 
+console.log(`Scores\nHuman:${humanScore}\t\tComputer:${computerScore}`);
+
+
 playGame()
+
+//Find way to stop responsiveness for a few seconds and then continuing the game after the score has been displayed.
+//Add CSS to stylise the page and center everything!
